4c34898f881dd107f7735173a4d004f9fe17ba7e
energetic-commit-5967 <jmcelroy@plexustech.net>
2020-04-17 19:58:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
-  base = { :railroads {} }
+  base = { railroads: {} }
   # keyword "return." Nice and neat.
   base 
 end
